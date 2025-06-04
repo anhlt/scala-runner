79fa39ea8800a31c5286111666358a5c1ade8584
anhlt <tuananh.kirimaru@gmail.com>
2025-06-04 02:51:10 +0000

    update

--- tests/test_main.py
+++ tests/test_main.py
@@ -54,5 +54,5 @@ def test_docker_error(monkeypatch):
         }
     )
     assert resp.status_code == 500
-    detail = resp.json().get("output", "")
+    detail = resp.json().get("detail", "")
     assert "Docker error!" in detail
